a2719c4a7f15a15f050918c10c012a36efcb9426
JasonGoGoGo <sunjason2016@163.com>
2017-04-04 18:07:27 +0800

    Update 字符串和编码.py

--- python learning/字符串和编码.py
+++ python learning/字符串和编码.py
@@ -2,4 +2,10 @@
 （2）ASCII编码是二进制码，1个字节，Unicode 国际标准编码，两个字节，UTF-8编码是可变字节长编码，如英文1个字节，汉字三个字节。
 （3）Python内置的一种数据类型是列表：list。list是一种有序的集合，可以随时添加和删除其中的元素。
 （4）一种有序列表叫元组：tuple。tuple和list非常类似，但是tuple一旦初始化就不能修改。
-  
+（5）一种是for...in循环，依次把list或tuple中的每个元素迭代出来；还有while
+  names = ['Michael', 'Bob', 'Tracy']
+  for name in names:
+      print(name);
+      
+ (6)Python内置了字典：dict的支持，dict全称dictionary，在其他语言中也称为map，使用键-值（key-value）存储，具有极快的查找速度。
+    请务必注意，dict内部存放的顺序和key放入的顺序是没有关系的。
