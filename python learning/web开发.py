2841ac1407153db388d9a141ac5bb1507d334f77
JasonGoGoGo <sunjason2016@163.com>
2017-04-05 23:07:28 +0800

    Create web开发.py

---
+++ python learning/web开发.py
@@ -0,0 +1,21 @@
+(1)首先要对HTML、CSS和JavaScript作一定的了解。HTML定义了页面的内容，CSS来控制页面元素的样式，而JavaScript负责页面的交互逻辑。
+<html>
+<head>
+  <title>Hello</title>
+  <style>
+    h1 {
+      color: #333333;
+      font-size: 48px;
+      text-shadow: 3px 3px 3px #666666;
+    }
+  </style>
+  <script>
+    function change() {
+      document.getElementsByTagName('h1')[0].style.color = '#ff0000';
+    }
+  </script>
+</head>
+<body>
+  <h1 onclick="change()">Hello, world!</h1>
+</body>
+</html>
